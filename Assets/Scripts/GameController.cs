e30b7ed525c1e9899a6d1a9606bf2610160175fb
masayume <masayume.blog@gmail.com>
2020-11-16 12:38:40 +0100

    cleanup

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,10 +14,6 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private FpsMovement player;
-    [SerializeField] private Text timeLabel;
-    [SerializeField] private Text scoreLabel;
-
     private MazeConstructor generator;
 
     private DateTime startTime;
@@ -40,7 +36,7 @@ public class GameController : MonoBehaviour
         startTime = DateTime.Now;
 
         score = 0;
-        scoreLabel.text = score.ToString();
+        // scoreLabel.text = score.ToString();
 
         StartNewMaze();
     }
@@ -52,10 +48,6 @@ public class GameController : MonoBehaviour
         float x = generator.startCol * generator.hallWidth;
         float y = 1;
         float z = generator.startRow * generator.hallWidth;
-        player.transform.position = new Vector3(x, y, z);
-
-        goalReached = false;
-        player.enabled = true;
 
         // restart timer
         timeLimit -= reduceLimitBy;
@@ -65,22 +57,24 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (!player.enabled)
         {
             return;
         }
+        */
 
         int timeUsed = (int)(DateTime.Now - startTime).TotalSeconds;
         int timeLeft = timeLimit - timeUsed;
 
         if (timeLeft > 0)
         {
-            timeLabel.text = timeLeft.ToString();
+            // timeLabel.text = timeLeft.ToString();
         }
         else
         {
-            timeLabel.text = "TIME UP";
-            player.enabled = false;
+            // timeLabel.text = "TIME UP";
+            // player.enabled = false;
 
             Invoke("StartNewGame", 4);
         }
@@ -92,7 +86,7 @@ public class GameController : MonoBehaviour
         goalReached = true;
 
         score += 1;
-        scoreLabel.text = score.ToString();
+        // scoreLabel.text = score.ToString();
 
         Destroy(trigger);
     }
@@ -102,7 +96,7 @@ public class GameController : MonoBehaviour
         if (goalReached)
         {
             Debug.Log("Finish!");
-            player.enabled = false;
+            // player.enabled = false;
 
             Invoke("StartNewMaze", 4);
         }
