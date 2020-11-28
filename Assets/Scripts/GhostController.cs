adc4c1c2c223870ac3d0d4fa92e5899fad219461
masayume <masayume.blog@gmail.com>
2020-11-28 17:29:22 +0100

    ghost move waypoints

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class GhostController : MonoBehaviour
 {
+    public Transform[] waypoints;
+    int cur = 0;
+    public float speed = 10f;
 
-    // private Text score;
 
     // Start is called before the first frame update
     void Start()
@@ -14,21 +16,32 @@ public class GhostController : MonoBehaviour
         // GetComponent<Animator>().SetFloat("DirY", 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    // use the FixedUpdate function to go closer to the current waypoint, 
+    // or select the next one as soon as we reached it
+    void FixedUpdate()
     {
+        if (transform.position != waypoints[cur].position) {
+            Vector2 p = Vector2.MoveTowards(transform.position, waypoints[cur].position, speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);
+        }
+        // Waypoint reached, select next one
+        else {
+            cur = (cur + 1) % waypoints.Length;
+        }
 
-
+        // Animation
+        Vector2 dir = waypoints[cur].position - transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
     }
-    /*
+
+
     void OnTriggerEnter2D(Collider2D co) {
-        if (co.name == "Player") {
-            Destroy(gameObject);    // dot eaten
-            Score.UpdateScore(10);  // score updated
+        if (co.name == "pacman") {
+            Destroy(gameObject);    // pacman dies
         }
 
 
     }
-    */
 
 }
