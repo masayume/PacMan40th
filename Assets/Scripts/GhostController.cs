323b6b94c0337793e5ad41bc99d67112a18a0b54
masayume <masayume.blog@gmail.com>
2020-11-28 01:57:11 +0100

    dark background + Ghost Controller

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+
+    // private Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // score = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    /*
+    void OnTriggerEnter2D(Collider2D co) {
+        if (co.name == "Player") {
+            Destroy(gameObject);    // dot eaten
+            Score.UpdateScore(10);  // score updated
+        }
+
+
+    }
+    */
+
+}
