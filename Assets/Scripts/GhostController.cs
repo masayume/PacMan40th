2bc584232294b7767ea5db246a2a90094f8351de
masayume <masayume.blog@gmail.com>
2020-12-27 19:29:41 +0100

    ops... gravity

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,8 +16,8 @@ public class GhostController : MonoBehaviour
     public int ghostState = 1; // start in chase mode = Ghost State: [0=frightened, 1=chase, 2=scatter, 3=eaten]
 
     private bool isMoving, moveDown, moveUp, moveLeft, moveRight;
-    private Vector2 origPos, targetPos, destPos;
-    private float timeToMove = 0.2f;
+    private Vector2 origPos, targetPos, destPos; // actor position; next move position; destination position (scatter/chase)
+    private float timeToMove = 1f;
     private float scale = 5f;
     public LayerMask moveBlockLayerMask;
 
@@ -50,7 +50,7 @@ public class GhostController : MonoBehaviour
 
     // use the FixedUpdate function to go closer to the current waypoint, 
     // or select the next one as soon as we reached it
-    void FixedUpdate()
+    void Update()
     {
 
         // evaluate available movements (set initial direction)
@@ -84,7 +84,7 @@ public class GhostController : MonoBehaviour
             for (int i=0; i<4; i++) // 0: up, 1: right, 2: down, 3: left
             {                
                 // Debug.Log("2 - calculate d2t[i] for valid moves");
-                // cheak any direction, if there are no walls and it's not a reverse
+                // check any direction, if there are no walls and it's not a reverse
                 if ( Valid(dir[i]) && !(lastDir == reverse[i]) ) {
                     maxDist = Vector2.Distance(origPos + dir[i], destPos);
                     if (maxDist < bestDist) 
@@ -93,36 +93,43 @@ public class GhostController : MonoBehaviour
                         moveDir = i;
                     }
                 } 
-                Debug.Log("act: " + actionCtr + "valid dir: " + i + "; rev: " + reverse[i] + " => val:" + Valid(dir[i]) );
             }
 
-            Debug.Log("act: " + actionCtr + " best dir: " + moveDir );
+            Debug.Log("act: " + actionCtr + " best: " + moveDir + "=== origPos:" + origPos + 
+            " Pos:" + transform.position.x + "," + transform.position.y);
 
             lastDir = moveDir;
 
-            if (moveDir == 0) moveUp = true;
-            else if (moveDir == 1) moveRight = true;
-            else if (moveDir == 2) moveDown = true;
-            else if (moveDir == 3) moveLeft = true;
-
-            if (moveDown) {
+            if (moveDir == 0) // 0: UP
+            {
+                Debug.Log("coroutine dir: " + moveDir + " pos: " + transform.position);
+                StartCoroutine(MoveGhost(Vector2.up));
+                // StartCoroutine(MoveGhost(transform.position, scale * Vector2.up));
+                anim.SetFloat("DirX", 0);
+                anim.SetFloat("DirY", 1.0f);                
+            }
+            else if (moveDir == 1) // 1: RIGHT
+            {
+                Debug.Log("coroutine dir: " + moveDir + " pos: " + transform.position);
+                StartCoroutine(MoveGhost(Vector2.right));
+                // StartCoroutine(MoveGhost(transform.position, scale * Vector2.right));
+                anim.SetFloat("DirX", 1.0f);
+                anim.SetFloat("DirY", 0);
+            }
+            else if (moveDir == 2) // 2: DOWN
+            {
+                Debug.Log("coroutine dir: " + moveDir + " pos: " + transform.position);
                 StartCoroutine(MoveGhost(Vector2.down));
+                // StartCoroutine(MoveGhost(transform.position, scale * Vector2.down));
                 anim.SetFloat("DirX", 0);
                 anim.SetFloat("DirY", -1.0f);
             }
-            if (moveUp) {
-                StartCoroutine(MoveGhost(Vector2.up));
-                anim.SetFloat("DirX", 0);
-                anim.SetFloat("DirY", 1.0f);
-            }
-            if (moveLeft) {
+            else if (moveDir == 3) // 3: LEFT
+            {
+                Debug.Log("coroutine dir: " + moveDir + " pos: " + transform.position);
                 StartCoroutine(MoveGhost(Vector2.left));
+                // StartCoroutine(MoveGhost(transform.position, scale * Vector2.left));
                 anim.SetFloat("DirX", -1.0f);
-                anim.SetFloat("DirY", 0);
-            }
-            if (moveRight) {
-                StartCoroutine(MoveGhost(Vector2.right));
-                anim.SetFloat("DirX", 1.0f);
                 anim.SetFloat("DirY", 0);
             }
 
@@ -140,24 +147,71 @@ public class GhostController : MonoBehaviour
 
     }
 
+/*
+    protected IEnumerator MoveGhostNew2(Vector2 nextPos)
+    {
+        float i = 0.0f;
+        float rate = 1 / moveDuration;
+        Vector3 newPos = nextCell.transform.position;
+        Vector3 startPos = transform.position;
+        while (i < 1)
+        {
+            i += rate * Time.deltaTime;
+            transform.position = Vector3.Lerp(startPos, newPos, i);
+            yield return null;
+        }
+        transform.position = newPos;
+        currentCell = nextCell;
+        Move();
+    }
+*/
+    
     private IEnumerator MoveGhost(Vector2 direction)
     {
         isMoving = true;
         float elapsedTime = 0;
+        float threshold = 0.005f;
+        float step;
+        float speed = 0.01f;
+
         origPos = new Vector2(transform.position.x, transform.position.y);
         targetPos = origPos + scale * direction;
 
+        step =  5f ; // calculate distance to move 
+
         while (elapsedTime < timeToMove)
         {
-            transform.position = Vector2.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
-            elapsedTime += Time.deltaTime;
-            yield return null;
+            if (Vector2.Distance(transform.position, targetPos) > threshold) {
+                transform.position = Vector2.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+                // elapsedTime += Time.deltaTime;
+                elapsedTime += Time.smoothDeltaTime;
+            } else {
+                transform.position = targetPos;
+            }
+            yield return new WaitForFixedUpdate();
+
         }
-        
-        Debug.Log("targetPos:" + targetPos);
-        transform.position = targetPos;
+      
+//        Debug.Log("targetPos final:" + targetPos);
+        // transform.position = targetPos;
 
         isMoving = false;
+
+        yield return null;
+    }
+
+
+    private IEnumerator MoveGhostNew(Vector2 from, Vector2 move) {
+            float speed = 1f;
+            Vector2 to = new Vector2(from.x + move.x, from.y + move.y);
+            float step = (speed / (from - to).magnitude) * Time.fixedDeltaTime;
+            float t = 0;
+            while (t <= 1.0f) {
+                t += step; // Goes from 0 to 1, incrementing by step each time
+                transform.position = Vector2.Lerp(from, to, t); // Move objectToMove closer to b
+                yield return new WaitForFixedUpdate();         // Leave the routine and return here in the next frame
+            }
+            transform.position = to;
     }
 
     bool Valid(Vector2 dir) {
